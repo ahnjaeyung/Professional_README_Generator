c5041613e1af39eaa888ae21ff86a1cef439c7b2
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-01-12 17:39:42 -0600

    added more links in table of contents

--- Develop/projectREADME.md
+++ Develop/projectREADME.md
@@ -1,28 +1,31 @@
-# Project Name  
-  ## Description  
-  This is a readme generator  
-  ## Table of Contents
-  If your README is long, add a table of contents to make it easy for users to find what they need.  
+# Title  
+## Description  
+Description  
+## Table of Contents
+If your README is long, add a table of contents to make it easy for users to find what they need.  
   - [Installation](#installation)  
   - [Usage](#usage)  
   - [Credits](#credits)  
   - [License](#license)  
-  ## Installation  
-  npm i  
-  ## Usage  
-  use it however you want  
+  - [Badges](#badges)
+  - [Features](#features)
+  - [How to Contribute](#how-to-contribute)
+## Installation  
+Installation  
+## Usage  
+Usage  
       ![alt text](assets/images/screenshot.png)  
-  ## Credits  
-  List your collaborators, if any, with links to their GitHub profiles.
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  If you followed tutorials, include links to those here as well.  
-  ## License  
-  MIT License  
-  ## Badges  
+## Credits  
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.  
+## License  
+MIT License  
+## Badges  
   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.  
-  ## Features  
-  If your project has a lot of features, list them here.  
-  ## How to Contribute  
-  don't contribute to my thing  
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.  
+## Features  
+If your project has a lot of features, list them here.  
+## How to Contribute  
+Contribution  
   
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,44 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}  
-  ## Description  
-  ${data.description}  
-  ## Table of Contents
-  If your README is long, add a table of contents to make it easy for users to find what they need.  
+## Description  
+${data.description}  
+## Table of Contents
+If your README is long, add a table of contents to make it easy for users to find what they need.  
   - [Installation](#installation)  
   - [Usage](#usage)  
   - [Credits](#credits)  
-  - [License](#license)  
-  ## Installation  
-  ${data.installation}  
-  ## Usage  
-  ${data.usage}  
+  - [License](#license) 
+  - [Badges](#badges)  
+  - [Features](#features)  
+  - [How to Contribute](#how-to-contribute)  
+## Installation  
+${data.installation}  
+## Usage  
+${data.usage}  
       ![alt text](assets/images/screenshot.png)  
-  ## Credits  
-  List your collaborators, if any, with links to their GitHub profiles.
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  If you followed tutorials, include links to those here as well.  
-  ## License  
-  ${data.license}  
-  ## Badges  
+## Credits  
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.  
+## License  
+${data.license}  
+## Badges  
   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.  
-  ## Features  
-  If your project has a lot of features, list them here.  
-  ## How to Contribute  
-  ${data.contributing}  
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.  
+## Features  
+If your project has a lot of features, list them here.  
+## How to Contribute  
+${data.contributing}  
   
 `;
 }
